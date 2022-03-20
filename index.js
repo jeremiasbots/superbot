cdc29f1a97ae4d268734c212c5a25671bc4d1aa0
jeremiasbots <victorrbravo@icloud.com>
2022-03-20 14:23:20 -0500

    Nuevo video

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 const Discord = require('discord.js')//Aqui definimos el modulo discord.js que acabamos de instalar con el npm i discord.js
 const client = new Discord.Client({ intents: 32509 })//Aqui definimos los eventos del bot, por asi decirlo los intents son los permisos del bot
 //En la descripcion dejare la pagina de los intents //Recomiendo poner 32767 ya que son todos los permisos
-const config = require('./config.json')//aqui definimos que la const config requiere el archivo config.json
+const config = require('./config.json')//Es más seguro el .env 
+require("dotenv").config()
+//Le decimos al bot que requiere dotenv que instalamos con npm i dotenv para configurar el .env
+
+
 
 client.on('ready', () => {
     console.log(`Sesion iniciada con node: ${process.version}`)
 })//Aqui definimos el evento ready con un console.log para que nos muestre un mensaje cuando inicie con la version de node
+
+//AVISO: para guardar cambios usa ctrl+s
 
 client.on('messageCreate', async(message) => {
     const prefix = config.prefix //Aca pedimos el texto llamado prefix
@@ -31,10 +37,21 @@ client.on('messageCreate', async(message) => {
         message.reply("Hola, soy un bot muy bueno")
     }//Aqui decimos que si el comando es igual a hola responda: Hola, soy un bot muy bueno
 
+    if(command === "ping"){
+        message.reply("Mi ping es de ** " + client.ws.ping + " ms**")
+
+        //comando ping sencillo
+        //client.ws.ping = el ping del bot
+    }
+
 
 })
 
-client.login(config.token)//Aca pedimos el texto llamado token
+client.login(process.env.TOKEN)
+//Pedimos la variable token mediante un proceso
+//y esa variable contiene el token/clave del bot
+
+//Funciona
 
 //El config.json sirve para definir cosas xd
 
